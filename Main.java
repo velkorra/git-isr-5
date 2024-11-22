1411d1f2726599c2f0abbf045d47ae1eada8e8a4
velkorra <anton.s.vertunov@gmail.com>
2024-11-22 16:26:23 +0300

    update main

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
